2742d41a1ffc213afd7e1bcb35c9f5b4c569c776
maximusMD <maximilian.dunne@gmail.com>
2023-10-02 13:38:29 +0100

    This commit will add the completed getTopics controller and model, as well as related testing

---
+++ __tests__/integration_testing.test.js
@@ -0,0 +1,32 @@
+const data = require('../db/data/test-data/index')
+const request = require('supertest')
+const seed = require('../db/seeds/seed')
+const db = require('../db/connection')
+const app = require('../app')
+
+beforeEach(() => seed(data))    
+afterAll(() => db.end())
+
+describe('General Errors', () => {
+    it('should return a 404 if not found', () => {
+        request(app).get('/api/notfound').expect(404).then((res) => {
+            expect(res.body.message).toBe('Not Found')
+        })
+    })
+});
+
+describe('getTopics', () => {
+    it('should return a 200 status code', () => {
+        return request(app).get("/api/topics").expect(200)
+    });
+    it('should return the topics', () => {
+        const expected = [
+            { slug: 'mitch', description: 'The man, the Mitch, the legend' },
+            { slug: 'cats', description: 'Not dogs' },
+            { slug: 'paper', description: 'what books are made of' }
+        ]
+        return request(app).get("/api/topics").then((res) => {
+            expect(res.body.topics).toEqual(expected)
+        })
+    });
+});
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const getTopics = require('./controllers/getTopics.controller')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/topics', getTopics)
+
+app.all('/*', (req, res, next) => {
+    res.status(404).send({message: 'Not Found'})
+})
+
+app.use((err, req, res, next) => {
+    if (err.code = '22P02'){
+        response.status(400).send({message: 'Bad Request'})
+    }
+    else if (err.status){
+        res.status(err.status).send({message: err.message})
+    }
+})
+
+
+module.exports = app
